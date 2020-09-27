adf75b86c7fd65083581cc090464cb4ab58f9525
angeldomp49 <angeldomp49@gmail.com>
2020-09-27 18:04:58 -0500

    create file

--- create.sql
+++ create.sql
@@ -3,6 +3,36 @@ CREATE DATABASE IF NOT EXISTS zoologic;
 
 USE zoologic;
 
+CREATE TABLE IF NOT EXISTS kinds(
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    latin_name VARCHAR(100),
+    description TEXT,
+    PRIMARY KEY (id)
+)
+ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS weathers(
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    description TEXT,
+    PRIMARY KEY (id)
+)
+ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS habitats(
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    weather_id BIGINT UNSIGNED NOT NULL,
+    name VARCHAR(100),
+    area DECIMAL(5,5),
+    inhabitats INT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_weather
+        FOREIGN KEY (weather_id)
+        REFERENCES weathers(id)
+)
+ENGINE = INNODB;
+
 CREATE TABLE IF NOT EXISTS animals(
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     habitat_id BIGINT UNSIGNED NOT NULL,
@@ -17,25 +47,12 @@ CREATE TABLE IF NOT EXISTS animals(
         FOREIGN KEY (habitat_id)
         REFERENCES habitats(id),
     CONSTRAINT fk_kind
-        FOREING KEY (kind_id)
+        FOREIGN KEY (kind_id)
         REFERENCES kinds(id)
 )
 ENGINE = INNODB;
 
-CREATE TABLE IF NOT EXISTS habitats(
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    weather_id BIGINT UNSIGNED NOT NULL,
-    name VARCHAR(100),
-    area DECIMAL(5,5),
-    inhabitats INT,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_weather
-        FOREING KEY (weather_id)
-        REFERENCES weathers(id)
-)
-ENGINE = INNODB;
-
-CREATE TABLE IF NOT EXISTS weathers(
+CREATE TABLE IF NOT EXISTS foods(
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
     description TEXT,
@@ -43,30 +60,21 @@ CREATE TABLE IF NOT EXISTS weathers(
 )
 ENGINE = INNODB;
 
-CREATE TABLE IF NOT EXISTS kinds(
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100),
-    latin_name VARCHAR(100),
-    description TEXT,
-    PRIMARY KEY (id)
-)
-ENGINE=INNODB;
-
-CREATE TABLE IF NOT EXISTS animal-food(
+CREATE TABLE IF NOT EXISTS animal_food(
     animal_id BIGINT UNSIGNED NOT NULL,
     food_id   BIGINT UNSIGNED NOT NULL,
     CONSTRAINT fk_animal
-        FOREING KEY (animal_id)
-        REFERENCES animals(id)
+        FOREIGN KEY (animal_id)
+        REFERENCES animals(id),
     CONSTRAINT fk_food
-        FOREING KEY (food_id)
+        FOREIGN KEY (food_id)
         REFERENCES foods(id)
 )
 ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS imageable(
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    photography MEDUIMBLOB,
+    photography MEDIUMBLOB,
     imageable_id BIGINT UNSIGNED NOT NULL,
     imageable_type VARCHAR(100),
     PRIMARY KEY (id)
