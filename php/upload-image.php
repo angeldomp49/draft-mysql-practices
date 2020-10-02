33163b3bdf654b1584a316f96cb0cff2041463de
angel dominguez <angeldomp49@gmail.com>
2020-10-02 16:37:08 -0500

    query

--- php/upload-image.php
+++ php/upload-image.php
@@ -1,5 +1,23 @@
 <?php
+$ANIMAL_TYPE = 1;
+$WEATHER_TYPE = 2;
 
-if($_POST['type'] == 1){
-    
+$image = $_POST['img'];
+$type  = $_POST['type'];
+$name_imageable = $_POST['name_imageable'];
+
+try {
+    $bdd = new PDO('mysql:host=localhost:3308;dbname=zoologic;charset=utf8;','root','');
+
+} catch (Exception $e) {
+    echo 'error';
+    $e->getMessage();
 }
+
+if($type == $ANIMAL_TYPE){
+    $bdd->exec('');
+}
+
+'INSERT INTO imageable VALUES(null, \''.$image.'\',
+(SELECT id FROM animals WHERE name = \''.$name_imageable.'\' LIMITS 1)
+,\'animal\');'
